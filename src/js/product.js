8a603c64c6d96e4b9ce0a6f0a60dd77135f8fb5b
MASTER_PRO <MASTER_PRO@MASTER>
2026-09-02 20:35:11 +0100

    completed 1

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,13 @@
-import { setLocalStorage } from "./utils.mjs";
+import {getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const storedCart = getLocalStorage("so-cart");
+  const cartItems = Array.isArray(storedCart) ? storedCart : [];
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
